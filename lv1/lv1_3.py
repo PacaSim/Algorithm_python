e83960d54eb8ba0694160c8580f9a4814dd6485c
JunHyeon Sim <116507879+PacaSim@users.noreply.github.com>
2023-04-05 23:10:03 +0900

    Update lv1_3.py

--- lv1/lv1_3.py
+++ lv1/lv1_3.py
@@ -1,4 +1,4 @@
-# 하샤드 수
+# 1. 하샤드 수
 def solution(x):
     str_x = str(x)
     answer = 0
@@ -9,3 +9,10 @@ def solution(x):
 # other code
 def Harshad(n):
     return n%sum(int(x) for x in str(n)) == 0
+
+# 2. 두 정수 사이의 합
+def solution(a, b):
+    if a > b:
+        return sum(list(range(b, a+1)))
+    else:
+        return sum(list(range(a, b+1)))
